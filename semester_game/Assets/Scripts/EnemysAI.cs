893e46832a0b5ff8a391904fa3306be93864a175
marechal-dev <pietro.developer@gmail.com>
2021-02-13 00:13:16 -0300

    DESGRAÇA DA IA FUNCIONANDO

--- semester_game/Assets/Scripts/EnemysAI.cs
+++ semester_game/Assets/Scripts/EnemysAI.cs
@@ -9,9 +9,11 @@ public class EnemysAI : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
-    public float health;
+    public ParticleSystem muzzleFlashAI;
 
-    //Patroling
+    public Animator animator;
+
+    // Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
@@ -19,52 +21,77 @@ public class EnemysAI : MonoBehaviour
     //Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
-    public GameObject projectile;
+    public float damage;
 
-    //States
+    // Chasing
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
 
     private void Awake()
     {
-        player = GameObject.Find("PlayerObj").transform;
+        player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        if(!playerInSightRange && !playerInAttackRange)
+        {
+            animator.SetBool("isWalking", true);
+            Patroling();
+        }
+
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetBool("isChasingPlayer", true);
+            ChasePlayer();
+        }
+
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
     }
 
     private void Patroling()
     {
-        if (!walkPointSet) SearchWalkPoint();
+
+        if(!walkPointSet)
+        {
+            Debug.Log("Searching for a Walk Point...");
+            SearchWalkPoint();
+        }
 
         if (walkPointSet)
+        {
+            Debug.Log("Walk Point Set: Going to walkPoint");
             agent.SetDestination(walkPoint);
+        }
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
-        //Walkpoint reached
         if (distanceToWalkPoint.magnitude < 1f)
+        {
+            Debug.Log("Distance to WalkPoint Less than 1f, walkPointSet = False");
             walkPointSet = false;
+        }
     }
+
     private void SearchWalkPoint()
     {
-        //Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
+        
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
             walkPointSet = true;
+        }
     }
 
     private void ChasePlayer()
@@ -74,37 +101,42 @@ public class EnemysAI : MonoBehaviour
 
     private void AttackPlayer()
     {
-        //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
-            ///Attack code here
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            ///End of attack code
+            ///Attack Code here
+            Shoot();
+            ///================
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
     }
+
+    private void Shoot()
+    {
+        muzzleFlashAI.Play();
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(transform.position, transform.forward, out hitInfo, attackRange))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            PlayerController target = hitInfo.transform.GetComponent<PlayerController>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
     private void ResetAttack()
     {
         alreadyAttacked = false;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-
-        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
-    }
-    private void DestroyEnemy()
-    {
-        Destroy(gameObject);
     }
 
     private void OnDrawGizmosSelected()
